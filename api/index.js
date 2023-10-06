a26558c8a4c5edf67c03d5789562c26cfb5697c5
Maxim <334890@gmail.com>
2023-10-06 10:23:09 +0600

    add cors

--- api/index.js
+++ api/index.js
@@ -1,4 +1,9 @@
-const app = require("express")();
+const jwt = require("jsonwebtoken");
+//
+const express = require("express");
+const app = express();
+//const app = require("express")();
+const { default: axios } = require("axios");
 const { v4 } = require("uuid");
 
 app.get("/api", (req, res) => {
@@ -13,6 +18,59 @@ app.get("/api/item/:slug", (req, res) => {
   res.end(`Item: ${slug}`);
 });
 
-app.listen(5000);
+app.get("/api/pb", (req, res) => {
+  async function fetchRecords() {
+    const response = await axios.get(
+      "https://grumpy-elephant.pockethost.io/api/collections/tasks/records"
+    );
+    return response.data;
+  }
+  fetchRecords().then((data) => res.json(data));
+  //res.end(`Item: ${slug}`);
+});
+///////////////////////////////////////////////////////////////////////////
+
+//const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+let refreshTokens = [];
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
+});
+
+app.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.token);
+  res.sendStatus(204);
+});
+
+app.post("/login", (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username;
+  const user = { name: username };
+
+  const accessToken = generateAccessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
+});
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "180s" });
+}
+
+//app.listen(4000);
+
+/////////////////////////////////////////////////////////////////////////////
+app.listen(2000);
 
 module.exports = app;
